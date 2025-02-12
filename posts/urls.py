396c97ac2d95ad0b4e67df0a40e38c4698ee7d3b
dimitargrigorov <dimitarfmi.si.23@gmail.com>
2025-02-12 18:28:13 +0100

    Aprove - rejet accoutn

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('message/', views.show_message, name="message"),
     path('welcome/', views.welcome, name="welcome"),
     path('profile/', views.profile, name="profile"),
+    path('approve_account/<int:message_id>/', views.approve_account, name='approve_account'),
+    path('reject_account/<int:message_id>/', views.reject_account, name='reject_account'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Post
-from account.models import MessageFromUser,MessageFromEmployee,Employee,CustomUser
+from account.models import MessageFromUser,MessageFromEmployee,Employee,CustomUser,Account
 # Create your views here.
 
 
@@ -35,5 +35,24 @@ def chose_action(request):
             pass
     else:
         return render(request, 'show_message.html')
+    
+
+def approve_account(request, message_id):
+    if request.method == "POST":
+        message = get_object_or_404(MessageFromUser, id=message_id)
+        account = Account(owner=message.user_from, bank=message.bank, balance=message.balance, user_number=message.user_number)
+        account.save()
+        message.delete()
+        return redirect("posts:message")
+
+    return redirect("posts:message")
+
+
+def reject_account(request, message_id):
+    if request.method == "POST":
+        message = get_object_or_404(MessageFromUser, id=message_id)
+        message.delete()
+        return redirect("posts:message")
+    return redirect("posts:message")
     
     
