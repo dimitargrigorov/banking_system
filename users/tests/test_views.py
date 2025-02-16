6610eaef11483e097d2cce841328e0cf3937a89b
dimitargrigorov <dimitarfmi.si.23@gmail.com>
2025-02-16 07:29:09 +0100

    Add more tests for users

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -35,5 +35,61 @@ class TestRegiterUser(TestCase):
     def test_register_user_not_metod_POST(self):
         response = self.client.get(reverse('users:register_user'))
         self.assertTemplateUsed(response, 'register_user.html')
+    
+    def test_register_employee_valid_form(self):
+        response = self.client.post(reverse('users:register_employee'),{
+            'username':'testuser',
+            'email':'testuser@gmail.com',
+            'egn':'4124123',
+            'age':24,
+            'password1':'Testtest2025!',
+            'password2':'Testtest2025!'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('posts:welcome'))
+
+    def test_register_employee_role_equal_to_employee(self):
+        response = self.client.post(reverse('users:register_employee'),{
+            'username':'testuser',
+            'email':'testuser@gmail.com',
+            'egn':'4124123',
+            'age':24,
+            'password1':'Testtest2025!',
+            'password2':'Testtest2025!'
+        })
+        self.assertEqual(Employee.objects.get(username='testuser').role, 'Служител')
+
+    def test_register_employee_not_metod_POST(self):
+        response = self.client.get(reverse('users:register_employee'))
+        self.assertTemplateUsed(response, 'register.employee.html')
+
+    def test_register_third_valid_form(self):
+        response = self.client.post(reverse('users:register_third_person'),{
+            'username':'testuser',
+            'email':'testuser@gmail.com',
+            'egn':'4124123',
+            'age':24,
+            'password1':'Testtest2025!',
+            'password2':'Testtest2025!'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('posts:welcome'))
+
+    def test_register_third_role_equal_to_third_person(self):
+        response = self.client.post(reverse('users:register_third_person'),{
+            'username':'testuser',
+            'email':'testuser@gmail.com',
+            'egn':'4124123',
+            'age':24,
+            'password1':'Testtest2025!',
+            'password2':'Testtest2025!'
+        })
+        self.assertEqual(CustomUser.objects.get(username='testuser').role, 'Трето лице')
+
+    def test_register_third_not_metod_POST(self):
+        response = self.client.get(reverse('users:register_third_person'))
+        self.assertTemplateUsed(response, 'register_third.html')
+
+    
 
 
--- users/views.py
+++ users/views.py
@@ -5,80 +5,80 @@ from django.contrib.auth import login, logout,authenticate
 from .forms import CustomUserCreationForm,EmployeeCreationForm, CreateEmployeeForm
 
 def register_user(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.role = 'Клиент'
-            user.set_password(form.cleaned_data["password1"])
+            user.set_password(form.cleaned_data['password1'])
             user.save()
-            user = authenticate(username=user.username, password=form.cleaned_data["password1"])
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
             if user is not None:
                 login(request, user)
-                return redirect("posts:welcome")
+                return redirect('posts:welcome')
             else:
-                form.add_error(None, "Грешка при аутентикация. Опитайте отново.")
+                form.add_error(None, 'Грешка при аутентикация. Опитайте отново.')
     else:
         form = CustomUserCreationForm()
-    return render(request, "register_user.html", {"form": form})
+    return render(request, 'register_user.html', {'form': form})
 
 
 def register_employee(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = EmployeeCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.role = 'Служител'
-            user.set_password(form.cleaned_data["password1"])
+            user.set_password(form.cleaned_data['password1'])
             user.save()
-            user = authenticate(username=user.username, password=form.cleaned_data["password1"])
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
             if user is not None:
                 login(request, user)
-                return redirect("posts:welcome")
+                return redirect('posts:welcome')
             else:
-                form.add_error(None, "Грешка при аутентикация. Опитайте отново.")
+                form.add_error(None, 'Грешка при аутентикация. Опитайте отново.')
     else:
         form = EmployeeCreationForm()
-    return render(request, "register.employee.html", {"form": form})
+    return render(request, 'register.employee.html', {'form': form})
 
 
 def register_third_person(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.role = 'Трето лице'
-            user.set_password(form.cleaned_data["password1"])
+            user.set_password(form.cleaned_data['password1'])
             user.save()
-            user = authenticate(username=user.username, password=form.cleaned_data["password1"])
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
             if user is not None:
                 login(request, user)
-                return redirect("posts:welcome")
+                return redirect('posts:welcome')
             else:
-                form.add_error(None, "Грешка при аутентикация. Опитайте отново.")
+                form.add_error(None, 'Грешка при аутентикация. Опитайте отново.')
     else:
         form = CustomUserCreationForm()
-    return render(request, "register_third.html", {"form": form})
+    return render(request, 'register_third.html', {'form': form})
 
 
 def login_view(request): 
-    if request.method == "POST":
+    if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid(): 
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
                 login(request, user)
-                return redirect("posts:welcome")
+                return redirect('posts:welcome')
             else:
-                form.add_error(None, "Невалидни данни за вход.")
+                form.add_error(None, 'Невалидни данни за вход.')
 
     else: 
         form = AuthenticationForm()
 
-    return render(request, "login.html", { "form": form })
+    return render(request, 'login.html', { 'form': form })
 
 
 def logout_view(request):
@@ -115,4 +115,4 @@ def add_employee(request):
             return redirect('posts:message')
     else:
         form = CreateEmployeeForm()
-    return render(request, 'add_employee.html', {"form": form})
+    return render(request, 'add_employee.html', {'form': form})
